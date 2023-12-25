aa298882e08960d28681b96a78cf0779197e6c1e
root <root@clear-teeth.aeza.network>
2023-12-26 00:49:14 +0300

    Some fixes

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-60oq9q#6-4e$_a)1d1y-6x-1b=7m52umgkiy3t&@i#!8f$f3db'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,5 +111,5 @@ STATIC_ROOT = BASE_DIR / 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_ROOT = BASE_DIR / "media/"
 MEDIA_URL = "/media/"
